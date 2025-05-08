b9d02a1f7c6b5bb843723dc2f020a3dfd83e410d
phenriqued <pedrodias3737@gmail.com>
2025-05-08 17:25:07 -0300

    adicionando limitador no textField e tratando divisões por 0

--- src/main/java/p_henriqued/javafxstudy/Controller/OperationController.java
+++ src/main/java/p_henriqued/javafxstudy/Controller/OperationController.java
@@ -1,15 +1,19 @@
 package p_henriqued.javafxstudy.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import p_henriqued.javafxstudy.util.Constraints;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.function.BiFunction;
 
 
-public class OperationController {
+public class OperationController implements Initializable {
 
     @FXML
     private TextField numberOneTextField;
@@ -21,18 +25,25 @@ public class OperationController {
 
     @FXML
     protected void onSumButtonClick() {
-        operation(Double::sum);
+        if(!emptyString()) operation(Double::sum);
     }
     @FXML
     protected void onSubtractButtonClick() {
-        operation((x, y) -> x - y);
+        if(!emptyString()) operation((x, y) -> x - y);
     }
     @FXML
     protected void onMultipleButtonClick() {
-        operation((x, y) -> x * y);
+        if(!emptyString()) operation((x, y) -> x * y);
     }
     @FXML
     protected void onDivisionButtonClick() {
+        if(emptyString()){
+           return;
+        }
+        if(numberSecondTextField.getText().equals("0")){
+            resultLabel.setText("Não é possível dividir por 0!");
+            return;
+        }
         operation((x, y) -> x / y);
     }
     @FXML
@@ -40,6 +51,14 @@ public class OperationController {
         numberOneTextField.setText("");
         numberSecondTextField.setText("");
         resultLabel.setText("");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constraints.setTextFieldDouble(numberOneTextField);
+        Constraints.setTextFieldDouble(numberSecondTextField);
+        Constraints.setTextFieldMaxLength(numberOneTextField, 10);
+        Constraints.setTextFieldMaxLength(numberSecondTextField, 10);
     }
 
     //Metodo resposável por fazer as operações utilizando a interface BiFunctional.
@@ -54,6 +73,10 @@ public class OperationController {
         }catch (NumberFormatException | ArithmeticException exception){
             AlertController.alertShow("ERROR", "ERROR PERFORMING THE OPERATION!", exception.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    private Boolean emptyString(){
+        return (numberOneTextField.getText().isEmpty() || numberSecondTextField.getText().isEmpty());
     }
 
 }
