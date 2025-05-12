9778fd845a1ef65c46dcf6ce2ad8ada225742668
phenriqued <pedrodias3737@gmail.com>
2025-05-12 12:01:13 -0300

    add About View for method onMenuItemAboutAction and some fixes

--- src/main/java/p_henriqued/javafxstudy/Controller/MainController.java
+++ src/main/java/p_henriqued/javafxstudy/Controller/MainController.java
@@ -1,16 +1,55 @@
 package p_henriqued.javafxstudy.Controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import p_henriqued.javafxstudy.MainApplication;
+import p_henriqued.javafxstudy.util.Alert.Alerts;
 
-public class MainController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
 
     @FXML
-    private MenuBar menuItemSeller;
+    private MenuItem menuItemSeller;
     @FXML
-    private MenuBar menuItemDepartment;
+    private MenuItem menuItemDepartment;
     @FXML
-    private MenuBar menuItemHelp;
+    private MenuItem menuItemAbout;
+
+    @FXML
+    protected void onMenuItemAboutAction(){
+        loadView("/p_henriqued/javafxstudy/gui/AboutView.fxml");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    private void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = loader.load();
+
+            Scene mainScene = MainApplication.getMainScene();
+            VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+            Node mainMenu = mainVbox.getChildren().getFirst();
+
+            mainVbox.getChildren().clear();
+            mainVbox.getChildren().add(mainMenu);
+            mainVbox.getChildren().addAll(newVbox.getChildren());
+        } catch (IOException e) {
+            Alerts.AlertShow("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
 
 
 }
