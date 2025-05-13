61817465aced737a84a5ab2450a6b9bae3d546dd
phenriqued <pedrodias3737@gmail.com>
2025-05-13 18:34:31 -0300

    add Validations Exceptions and some adjustment

---
+++ src/main/java/p_henriqued/javafxstudy/infra/Exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package p_henriqued.javafxstudy.infra.Exceptions;
+
+public class ValidationException extends RuntimeException {
+  public ValidationException(String message) {
+    super(message);
+  }
+}
